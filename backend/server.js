1b6a014df6c28f11dbad6df3d3d6c9af3e88609f
suzunachan <akikoy143@gmail.com>
2026-03-09 03:06:57 +0800

    fix CORS

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const multer = require("multer");
 const path = require("path");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://pulse-tracker-eosin.vercel.app',
+    credentials: true
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
